f1156f5a90341ee34c0d048df262761e2c9093d4
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2013-03-26 23:03:37 +0800

    fixed user edit password confirmation field

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable#, :confirmable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  attr_accessible :email, :ic_number, :name, :password, :phone, :username
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :current_password
+  attr_accessible :email, :ic_number, :name, :password, :phone, :username, :encrypted_password
   attr_accessible :address1, :address2, :city, :postcode, :state, :avatar
 
   mount_uploader :avatar, AvatarUploader
