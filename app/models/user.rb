05d43404410496fd2eaa7a3ef89cc4d7feda63b6
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2013-03-29 11:07:07 +0800

    fixed email confirmation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
     self.confirmed ||= 'false'
   end
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable#, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
