87292ac43fa193e4e441dc551262f744fb475153
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2013-03-29 12:16:35 +0800

    updated gitignore to not ignore Gemfile. udpated root to login path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ BizPkns::Application.routes.draw do
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout", sign_up: "signup"}
 
   resources :users
-  root :to => "users#index"
+  root :to => redirect('/users/login')
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
