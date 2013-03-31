5d4b55a841df05209beebdf147989a7285c07f24
Firdaus Adib <i@firdaus.my>
2013-03-31 14:00:56 +0800

    Modify shedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 set :output, "log/cron.log"
 
-every 1.months, :at=> '1am' do
+every 1.minutes do
   #rake 'lib/tasks/cleanup_user.rake'
   command 'User.where("confirmed = 0 AND created_at <= ?", 6.months.ago).delete'
 end
