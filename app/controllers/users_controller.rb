782c5ac78a1d0ac2fecdc53629ce64917925d18f
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2013-03-31 16:59:36 +0800

    fixed business photo and logo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,7 +57,7 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(params[:user])
-    @business_profile = @user.build_business_profile
+    #@business_profile = @user.build_business_profile
 
     respond_to do |format|
       if @user.save
