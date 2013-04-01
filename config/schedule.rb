a4a5964b4af7084549f37ff9cba0fd2902c6b75d
Firdaus Adib <i@firdaus.my>
2013-04-01 20:34:05 +0800

    Push scheduler first. Not done yet

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,7 @@
+
 set :output, "log/cron.log"
 
-every 1.minutes do
+every 1.minutes, :environment => :development do
   #rake 'lib/tasks/cleanup_user.rake'
   command 'User.where("confirmed = 0 AND created_at <= ?", 6.months.ago).delete'
 end
