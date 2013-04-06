eb4e6a4a7d4648d94b6c8626c4fd21582c7dcd1c
Firdaus Adib <i@firdaus.my>
2013-04-06 14:51:05 +0800

    email set to pkns@iedwrites.com for now

--- app/mailers/consultant_mailer.rb
+++ app/mailers/consultant_mailer.rb
@@ -1,5 +1,5 @@
 class ConsultantMailer < ActionMailer::Base
-  default from: "admin@pknsbizclub.com"
+  default from: "pkns@iedwrites.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
