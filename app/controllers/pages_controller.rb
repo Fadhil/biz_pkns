961f6a77b75ae3adf36abc30b6eb18b58303e103
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2013-04-09 00:16:36 +0800

    search business directory for business users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,12 +14,21 @@ class PagesController < ApplicationController
       unless params[:search][:category] == ''
         category = params[:search][:category]
       end
+      unless params[:search][:city] == ''
+        city = params[:search][:city]
+      end
       if category && !state
         @users = User.joins(:business_profile).where('business_profiles.category' => category)
-      elsif state && !category
-        @users = User.joins(:business_profile=>:address).where('addresses.state' => state)
+      elsif (state || city) && !category
+        if !city
+          @users = User.joins(:business_profile=>{:address=>:city}).where('cities.state_name'=>state)
+        else
+          @users = User.joins(:business_profile=>{:address=>:city}).where('cities.id'=>city)
+        end
       elsif category && state
-        @users = User.joins(:business_profile=>:address).where('addresses.state' => state, 'business_profiles.category' => category)
+        @users = User.joins(:business_profile=>{:address=>:city}).where('cities.state_name' => state, 'business_profiles.category' => category)
+      else
+        @users = User.all
       end
     else
       @users = User.joins(:business_profile)
