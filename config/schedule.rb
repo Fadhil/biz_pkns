025e3b73566d074660d9ee89fbae1c99000982ab
Firdaus Adib <i@firdaus.my>
2013-04-11 01:01:44 +0800

    Add contact button in consultant profile

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,7 @@
-set :output, "log/cron.log"
+set :output, "#{path}/log/cron.log"
 
-every 1.minutes, :environment => :development do
+every 2.minutes, :environment => :development do
   #rake 'lib/tasks/cleanup_user.rake'
   command 'User.where("confirmed = 0 AND created_at <= ?", 6.months.ago).delete'
+  #runner 'User.where("confirmed = 0 AND created_at <= ?", 6.months.ago).delete'
 end
