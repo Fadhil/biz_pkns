bca733b71a1c120880b208822a5643be2a79c29f
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2013-04-12 00:29:00 +0800

    add current_employment_status to user. updated form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130411153915) do
+ActiveRecord::Schema.define(:version => 20130411161718) do
 
   create_table "addresses", :force => true do |t|
     t.string   "line1"
@@ -116,14 +116,14 @@ ActiveRecord::Schema.define(:version => 20130411153915) do
   create_table "users", :force => true do |t|
     t.string   "ic_number"
     t.string   "phone"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
+    t.string   "email",                     :default => "", :null => false
+    t.string   "encrypted_password",        :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",             :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -141,6 +141,7 @@ ActiveRecord::Schema.define(:version => 20130411153915) do
     t.string   "gender"
     t.string   "twitter_handle"
     t.string   "facebook_handle"
+    t.string   "current_employment_status"
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
