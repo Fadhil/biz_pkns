3edb46587a41a0bd11f5066b481206a202182737
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2013-04-22 00:14:42 +0800

    merged and fixed conflicts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,20 @@ class ApplicationController < ActionController::Base
     redirect_to root_url
   end
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
+    
   def city_dropdown
     @cities = City.where(state_name: params[:state])
     respond_to do |format|
       format.html { render layout: false }
     end
   end
+
+  def current_ability
+    @current_ability ||= Ability.new(current_user)
+  end
+
+
 end
