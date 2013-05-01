e4015f2c522f08494149e1976aa0382ed127dfc5
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2013-05-02 02:01:10 +0800

    Locales fix. Some custom css.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -51,7 +51,7 @@ class CoursesController < ApplicationController
 
     respond_to do |format|
       if @course.save
-        format.html { redirect_to @course, notice: 'Course was successfully created.' }
+        format.html { redirect_to @course, notice: t('successfully_created',resource: t('course')) }
         format.json { render json: @course, status: :created, location: @course }
       else
         format.html { render action: "new" }
@@ -67,7 +67,7 @@ class CoursesController < ApplicationController
 
     respond_to do |format|
       if @course.update_attributes(params[:course])
-        format.html { redirect_to @course, notice: 'Course was successfully updated.' }
+        format.html { redirect_to @course, notice: t('successfully_updated'),resource: t('course') }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
