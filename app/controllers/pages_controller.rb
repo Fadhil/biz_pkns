45c463f839ae575b4e8c6cb1ddc72b7f1b46150e
Firdaus Adib <i@firdaus.my>
2013-06-15 07:38:39 +0800

    Upcoming courses sort by nearest date

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,12 +13,12 @@ class PagesController < ApplicationController
     if params[:search].present?
       if params[:search][:terms].present?
         search_terms = params[:search][:terms].split(' ').join('%')
-        @courses = Course.active.upcoming.where('LOWER(name) like ?',"%#{search_terms}%").page(params[:page])
+        @courses = Course.active.upcoming.where('LOWER(name) like ?',"%#{search_terms}%").page(params[:page]).order("start_date DESC")
       else
-        @courses = Course.active.upcoming.page(params[:page])
+        @courses = Course.active.upcoming.page(params[:page]).order("start_date DESC")
       end
     else
-      @courses = Course.active.upcoming.page(params[:page])
+      @courses = Course.active.upcoming.page(params[:page]).order("start_date DESC")
     end
   end
 
