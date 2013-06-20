28b0eeeb8a9cbb4feebb908c53abd1f1aefc359f
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2013-06-20 23:05:57 +0800

    Only allow CSV uploads.

--- app/controllers/past_attendances_controller.rb
+++ app/controllers/past_attendances_controller.rb
@@ -18,29 +18,33 @@ class PastAttendancesController < ApplicationController
   def create
     PastAttendance.make_all_old
     data = params[:past_attendance][:file].read
-    @parsed_file=CSV.parse(data)
-    n=0
-    completed = true
-    @parsed_file.each  do |row|
-      unless row[0] == 'No'
-        c=PastAttendance.where('course = ? AND ic_number = ?',row[2], row[8]).first || PastAttendance.new
-        c.program=row[1]
-        c.course=row[2]
-        c.start_date=row[3]
-        c.end_date=row[4]
-        c.venue= row[5]
-        c.first_name = row[6]
-        c.last_name = row[7]
-        c.ic_number = row[8]
-        c.phone = row[9]
-        c.email = row[10]
-        c.successful = false
-        c.latest_uploaded = true
-        if c.save
-          n=n+1
-          GC.start if n%50==0
-        else 
-          completed = false
+    extension = params[:past_attendance][:file].content_type
+    completed = false
+    if extension == 'text/csv'
+      @parsed_file=CSV.parse(data)
+      n=0
+      completed = true
+      @parsed_file.each  do |row|
+        unless row[0] == 'No'
+          c=PastAttendance.where('course = ? AND ic_number = ?',row[2], row[8]).first || PastAttendance.new
+          c.program=row[1]
+          c.course=row[2]
+          c.start_date=row[3]
+          c.end_date=row[4]
+          c.venue= row[5]
+          c.first_name = row[6]
+          c.last_name = row[7]
+          c.ic_number = row[8]
+          c.phone = row[9]
+          c.email = row[10]
+          c.successful = false
+          c.latest_uploaded = true
+          if c.save
+            n=n+1
+            GC.start if n%50==0
+          else 
+            completed = false
+          end
         end
       end
     end
@@ -52,7 +56,11 @@ class PastAttendancesController < ApplicationController
       end
       notice = t('upload_successful')
     else 
-      notice = t('upload_unsuccessful')
+      if extension != 'text/csv'
+        notice = t('please_upload_csv')
+      else
+        notice = t('upload_unsuccessful')
+      end
     end
 
     respond_to do |format|
