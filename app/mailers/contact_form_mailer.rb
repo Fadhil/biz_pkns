cf4e926987bfbf2094c243ce7ae9dd21ec1ed07c
Firdaus Adib <i@firdaus.my>
2013-06-28 00:51:36 +0800

    Contact form in contact page done

--- app/mailers/contact_form_mailer.rb
+++ app/mailers/contact_form_mailer.rb
@@ -6,6 +6,6 @@ class ContactFormMailer < ActionMailer::Base
     @email = email
     @content = content
 
-    mail to: "epi.skywalker@gmail.com", subject: "Contact", cc: "1.shahida@gmail.com" #, from: %("#{@emailname}" <#{@emailsender}>)
+    mail to: "admin@pknsbizclub.com", subject: "Contact", cc: "1.shahida@gmail.com" #, from: %("#{@emailname}" <#{@emailsender}>)
   end
 end
