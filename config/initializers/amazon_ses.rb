46f1839abe39fdc5b82c60ca1f0f0d95ef5087f4
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2013-06-30 12:40:37 +0800

    amazon ses moved keys back to init file

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,3 +1,3 @@
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-  access_key_id: ENV['AMAZON_ACCESS_KEY'], #'AKIAJNHIISHPT6R4MCKA',#
-  secret_access_key: ENV['AMAZON_SECRET_KEY'] #'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'#
+  access_key_id: 'AKIAJNHIISHPT6R4MCKA',#
+  secret_access_key: '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'#
