e842445840adb2c0b0fdbbf53f8a0ee356c22cea
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2013-07-02 00:37:17 +0800

    portal home shows slider

--- app/assets/javascripts/banner_slider.js
+++ app/assets/javascripts/banner_slider.js
@@ -1,12 +1,13 @@
 $(function() {
       $('#slides').slidesjs({
         width: 940,
-        height: 528,
+        height: 220,
         play: {
           active: true,
           auto: true,
           interval: 4000,
           swap: true
-        }
+        },
+        pagination: false
       });
     });
