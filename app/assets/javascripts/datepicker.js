d73da963ec1410045c1e300773a3736d09858132
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2013-07-15 19:45:58 +0800

    Calendar bugs. Cannot choose date before today. End date cannot be less than start date.

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
   $('.datepicker').datepicker({
-      dateFormat: "yy-mm-dd"
+      dateFormat: "yy-mm-dd",
+      minDate: new Date()
     });
+
+  $('#start_date').change(function(){
+    m_date = $(this).datepicker('getDate');
+    min_date = new Date(m_date.getTime());
+    min_date.setDate(min_date.getDate() + 1);
+
+    $('#end_date').datepicker("option", "minDate", min_date);
+  });
 });
+
+
