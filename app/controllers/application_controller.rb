de09b7e1dbdaed1090eabbc5e90b8d5d109477b1
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2013-07-27 01:34:17 +0800

    Admins go to user login page upon logout.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,8 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     if resource == ('consultant'.to_sym)
       new_consultant_session_url
+    elsif resource.admin?
+      new_user_session_url
     else
       root_url
     end
