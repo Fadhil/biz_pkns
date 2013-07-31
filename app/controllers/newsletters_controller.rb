438bbfaba50a7c9b987b9a7d4058477173b02432
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2013-07-31 14:11:33 +0800

    Bulletin go to preview upon update after kemaskini.

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -104,7 +104,7 @@ class NewslettersController < ApplicationController
 
     respond_to do |format|
       if @newsletter.update_attributes(params[:newsletter])
-        format.html { redirect_to @newsletter, notice: 'Newsletter was successfully updated.' }
+        format.html { redirect_to preview_newsletter_path(@newsletter), notice: 'Buletin berjaya dikemaskini.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
