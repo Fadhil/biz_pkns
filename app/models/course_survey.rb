b0138c5a44f1ab2d7da1a1d15585bea9940bb873
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2013-08-26 17:45:43 +0800

    Consultant Course Report WIP

--- app/models/course_survey.rb
+++ app/models/course_survey.rb
@@ -5,28 +5,33 @@ class CourseSurvey < Survey
 
   def import_survey_data(data)
     @parsed_file=CSV.parse(data)
-    @parsed_file.each  do |row|
-      if row[0] == 'Jumlah Penilaian Peserta'
+    @parsed_file.each_with_index  do |row,row_index|
+      if row_index == 0
         no_columns = row.size
         row.each_with_index do |column, index|
-          self.questions << Question.create(title: column) unless index == 0
+          if index == no_columns - 1
+            self.questions << Question.create(title: column, question_type: 'yes_no')
+          else
+
+            self.questions << Question.create(title: column, question_type: 'scale')
+          end
         end
       elsif row[0] != nil
         row.each_with_index do |column, index|
 
           # We're ignoring the first column, so question[0] is actually question[index - 1] (starting from index 1)
-          unless index == 0
-            question = self.questions[index - 1]
+   
+            question = self.questions[index]
             question.responses << Response.create(answer: column) 
-            if question.question_type == nil
-              if column == 'ya'
-                question.question_type = 'yes_no'
-              else
-                question.question_type = 'scale'
-              end
-              question.save
-            end
-          end
+            # if question.question_type == nil
+            #   if column == 'ya'
+            #     question.question_type = 'yes_no'
+            #   else
+            #     question.question_type = 'scale'
+            #   end
+            #   question.save
+            # end
+       
         end
       end
     end
