e4f5eb37ef9a853697f262f15d71620b085108a3
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2013-09-12 03:27:40 +0800

    End date can be on same day.

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
   $('#start_date').change(function(){
     m_date = $(this).datepicker('getDate');
     min_date = new Date(m_date.getTime());
-    min_date.setDate(min_date.getDate() + 1);
+    min_date.setDate(min_date.getDate());
 
     $('#end_date').datepicker("option", "minDate", min_date);
   });
