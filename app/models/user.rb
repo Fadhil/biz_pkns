2507ea6dfe5fd6e742ff65c92bf3b47b1014e8f4
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2013-09-19 06:49:39 +0800

    nonadmin users, how do we scope this?

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
 
-  scope :nonadmin, includes(:roles).where('roles.id is null')
+  scope :nonadmin, includes(:roles_user).where('roles_users.role_id is null')
   scope :members, joins(:membership)
   scope :nonmembers, joins('left outer join memberships on users.id = memberships.user_id').where('memberships.id is null')
   scope :male, where(gender: 'm')
