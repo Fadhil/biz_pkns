872c2a3d3ce299e54d744985cc1f8f8b198dc612
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2013-11-06 13:41:50 +0800

    some formattings on the pdf generator

--- app/pdfs/letter_pdf.rb
+++ app/pdfs/letter_pdf.rb
@@ -2,25 +2,46 @@ class LetterPdf < Prawn::Document
 include ApplicationHelper
   def initialize(letter, sender, users)
     
-    super(top_margin: 140)
+    super(top_margin: 10)
+   
+
     users.each do |user|
+        font "Helvetica", style: :bold
+        text '"KEJUJURAN DAN KETEKUNAN"'
+
+        font "Helvetica",style: :normal
+        draw_text "Surat Kami:", at: [400,700]
+        draw_text "Tarikh: #{letter.date}", at: [400, 680]
+        move_down 100
+
         text "#{user.full_name}"
+        move_down 20
+
         text full_address_text(user)
-
         move_down 20
-        text "#{letter.date}"
 
+
+        text "Tuan/Puan,"
         move_down 20
+
+
+        font "Helvetica", style: :bold
         text "Perkara: #{letter.subject},"
-
+        font "Helvetica", style: :normal
         move_down 15
         text "#{letter.message}".gsub("<p>","").gsub("</p>","\n\n").gsub("<br>","\n\n").gsub("<div>","").gsub("</div>","").gsub("&nbsp;"," ").html_safe, :inline_format => true
 
         move_down 20
-        text "Yang Bertugas,".html_safe
 
-        move_down 3
-        text sender.full_name
+        font "Helvetica", style: :bold
+        text '"BERKHIDMAT UNTUK NEGARA"'
+        font "Helvetica", style: :normal
+        move_down 10
+ 
+        text "Saya yang menurut perintah"
+
+        # move_down 3
+        # text sender.full_name
         start_new_page
     end
   end
