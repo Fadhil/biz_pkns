01b08fc318cd47694b28abc2f2b3c037f51bd98e
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2013-12-03 18:28:05 +0800

    no link_to in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   before_filter :authenticate_the_thing
-  #before_filter :check_status
+  before_filter :check_status
 
   protect_from_forgery
 
@@ -61,20 +61,20 @@ class ApplicationController < ActionController::Base
       if current_user.walk_in_first_time?
         flash.now[:notice] = t('please_change_default_password')
       elsif current_user.membership.nil? && !current_user.admin?
-          flash.now[:notice] = t('please_become_a_member', href: link_to('sini', edit_user_path(current_user)))
+          flash.now[:notice] = t('please_become_a_member', href: "<a href='#{edit_user_path(current_user)}'>sini</a>"
       elsif current_user.confirmed && !current_user.profile_complete? 
         unless current_user.admin?
           flash.now[:notice] = t('please_complete_your_profile')
         end
       elsif !current_user.unfinished_surveys.empty?
         unless current_user.admin?
-          flash.now[:notice] = t('you_have_new_surveys_here') + " <a href='#{surveys_user_path(current_user)}''>Jawab Survey</a>"
+          flash.now[:notice] = t('you_have_new_surveys_here') + " <a href='#{surveys_user_path(current_user)}'>Jawab Survey</a>"
         end
       end
     end
     if current_consultant
       if !current_consultant.profile_complete?
-        flash.now[:notice] = t('consultant_please_complete_profile', href: link_to('sini', edit_consultant_path(current_consultant)) )
+        flash.now[:notice] = t('consultant_please_complete_profile', href: "<a href='#{edit_consultant_path(current_consultant)}'>sini</a>"
       end
     end
   end
