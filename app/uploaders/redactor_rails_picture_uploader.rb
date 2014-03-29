8598e69da76df36574ee7dd4d227e423c46624da
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2014-03-29 18:11:10 +0800

    picture uploader directory

--- app/uploaders/redactor_rails_picture_uploader.rb
+++ app/uploaders/redactor_rails_picture_uploader.rb
@@ -8,6 +8,9 @@ class RedactorRailsPictureUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :fog
 
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 
   process :read_dimensions
 
