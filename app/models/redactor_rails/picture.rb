a54e780f2dc93b55c818928ddaa1b673000a6619
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2014-04-08 07:19:27 +0800

    try this url_content thing for redactor to get s3 to work

--- app/models/redactor_rails/picture.rb
+++ app/models/redactor_rails/picture.rb
@@ -2,6 +2,6 @@ class RedactorRails::Picture < RedactorRails::Asset
   mount_uploader :data, RedactorRailsPictureUploader, :mount_on => :data_file_name
 
   def url_content
-    url(:content)
+    url(:content) + '?AWSAccessKeyId=ASIAIE5DMA4YI2EWA5YQ&Expires=1396913007&Signature=3Tn/NX2Zdhxx1t0e8xSpeEMFzBs%3D&x-amz-security-token=AQoDYXdzEJj//////////wEakALCmuIVebn/c0Lvtk1N6uh1yi895z6B%2BVOBambev2MAm/T1Idswr2NYxvAiU7PH7R1Og7mgvyCl/fwl%2BvWNVgHtiD37stUwTHb%2BG9%2B9wg6Wa8A40Nxi7Rl7nKPPN4n%2BbUfAg%2BW/wxk0UiMNoipNL1GQ1kHpSdrJ15wPyC9bncgobZvHuu72C1JgXeS2SXg9Pse%2BwteK56%2BQrdA9kRuwqzXypr7XyKznt7sF7EXVfrJo/J9z8TIZfsObVUH06NAjOuqTlWVI1CnN7M%2BmFlLXhTHPc9QtvU9hfOKhi7x9lCp6yLey5OD5%2By6aK4cMCwAPvQCCk3CvNotrB9XgAz4SnQT7uxSPk/le3cA5mO7jJLaYqSCdy4yaBQ%3D%3D'
   end
 end
